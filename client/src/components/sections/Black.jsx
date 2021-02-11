98ee940371d63443a379053f3daace8fffbc58b0
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-02-11 19:54:16 +0000

    black section fixed

--- client/src/components/sections/Black.jsx
+++ client/src/components/sections/Black.jsx
@@ -14,23 +14,23 @@ function Black(props){
       </div> 
       <div class='container-black'>
         <div class='title-box'>
-          <h1 class='title-box'>PERKS</h1>
+          <h1 class='title-box-ttl'>PERKS</h1>
         </div>
         <div class='boxes'>
-          <div class='box-benefits'>
+          <div class='box-benefits-black'>
             <hr id='bnf-red-line'/>
             <h4 class='box-benefits' id='box-benefits-red'>Subscription Payment Model</h4>
-            <p class='benefits-txt-white'>No commitment, cancel anytime. Never worry about forgetting a payment again</p>
+            <p class='benefits-txt-white'>No commitment, cancel anytime. Never worry about forgetting a payment again!</p>
           </div>
-          <div class='box-benefits'>
+          <div class='box-benefits-black'>
             <hr id='bnf-blue-line'/>
             <h4 class='box-benefits' id='box-benefits-blue'>No Fee Cancelation Policy</h4>
-            <p class='benefits-txt-white'>No commitment, cancel anytime. Never worry about forgetting a payment again</p>
+            <p class='benefits-txt-white'>No commitment, cancel anytime. Never worry about forgetting a payment again!</p>
           </div>
-          <div class='box-benefits'>
+          <div class='box-benefits-black'>
             <hr id='bnf-yellow-line'/>
             <h4 class='box-benefits' id='box-benefits-yellow'>Subscription Payment Model</h4>
-            <p class='benefits-txt-white'>No commitment, cancel anytime. Never worry about forgetting a payment again</p>
+            <p class='benefits-txt-white'>No commitment, cancel anytime. Never worry about forgetting a payment again!</p>
           </div>
         </div>
       </div> 
